07f5e4b49c1746bf17af2f7f764ce7bf714daa15
Cesar Garcia Moreno <01cesargm@gmail.com>
2023-06-01 18:00:41 +0200

    a

---
+++ archivo.py
@@ -0,0 +1,11 @@
+# archivo.py
+
+def obtener_datos():
+    # Realiza alguna lógica para obtener los datos que deseas devolver
+    datos = "Estos son los datos que devuelve el archivo Python."
+    return datos
+
+# Punto de entrada del programa
+if __name__ == '__main__':
+    resultado = obtener_datos()
+    print(resultado)
